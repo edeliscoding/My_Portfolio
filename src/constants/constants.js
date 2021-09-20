76d6fc9894fa311a86244793c9a3bdb21a23bc4d
edeliscoding <edelreichd@gmail.com>
2021-09-19 19:53:31 -0600

    next js react

---
+++ src/constants/constants.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: "MERN Blog",
+    description:
+      "This project is buing using React with Context, Node.js, Express & MongoDB. This is a full stack application with Login and Register functionality where a user can create their own blog and view others. Users can only edit their own blog. They can also change their profile settings.",
+    image: "/images/my personal blog.png",
+    tags: ["Mongo", "Express", "React", "Node"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 0,
+  },
+  {
+    title: "Covid-19 Tracker",
+    description:
+      "This project was created during the height of the pandemic. The graphs show infected, recovered, deaths for each country and are in real-time",
+    image: "/images/covid cases.png",
+    tags: ["React", "JavaScript"],
+    source: "https://covid19dailytracker.netlify.app/",
+    visit: "https://covid19dailytracker.netlify.app/",
+    id: 1,
+  },
+  {
+    title: "The Venue",
+    description:
+      "Website for concert lovers. This website shows upcoming concerts or events. It includes a countdown so people don't miss out. It was built using React and latest technology for animations.",
+    image: "/images/The Venue.png",
+    tags: ["React", "Javascript"],
+    source: "https://edeliscoding.github.io/The_Venue/",
+    visit: "https://edeliscoding.github.io/The_Venue/",
+    id: 2,
+  },
+  {
+    title: "Outdoor Tours",
+    description:
+      "A website for booking beautiful outdoor tours. This project was built using HTML, CSS, SASS, Javascript. The focus of this website is the design using CSS.",
+    image: "/images/outdoor tours.png",
+    tags: ["HTML", "CSS"],
+    source: "https://edeliscoding.github.io/NaTours.github.io/",
+    visit: "https://edeliscoding.github.io/NaTours.github.io/",
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
